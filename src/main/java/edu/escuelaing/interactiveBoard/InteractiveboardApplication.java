f7cfbc29541c5f18a43def1756239ea77520abec
Ernesto Camacho <ernesto.camachoa9805gmail.com>
2021-06-29 12:00:23 -0500

    Primer commit

---
+++ src/main/java/edu/escuelaing/interactiveBoard/InteractiveboardApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.interactiveBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/interactiveBoard/InteractiveboardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.interactiveBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
